c90303fc455c147ce93d59edc66cd741d9fbeafa
Ellis Madagan <ellismadagan@gmail.com>
2026-04-11 19:45:20 -0400

    docs: add todo

--- app/core/db.py
+++ app/core/db.py
@@ -19,15 +19,6 @@ def init_db(session: Session) -> None:
     # This works because the models are already imported and registered from app.models
     # SQLModel.metadata.create_all(engine)
 
-    # user = session.exec(
-    #     select(User).where(User.email == settings.FIRST_SUPERUSER)
-    # ).first()
-    # if not user:
-    #     user_in = UserCreate(
-    #         email=settings.FIRST_SUPERUSER,
-    #         password=settings.FIRST_SUPERUSER_PASSWORD,
-    #         is_superuser=True,
-    #     )
-    #     user = crud.create_user(session=session, user_create=user_in)
+    # TODO: add seed data, not really necessary for now
 
     pass
